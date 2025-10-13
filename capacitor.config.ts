6951987952e14c4086e4da8b6203780718417402
Devlop1327 <primevideo@gmail.com>
2025-10-13 18:52:12 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.reciclatura.app',
+  appName: 'recicla-tura',
+  webDir: 'www'
+};
+
+export default config;
